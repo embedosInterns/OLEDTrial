fb6ab42d89c44ce09beae3efddacd77b5e8ce8e5
interns_embedos <interns@embedos.io>
2017-03-24 17:27:11 +0530

    Modified startup screen text

--- i2c_display_test/main.c
+++ i2c_display_test/main.c
@@ -209,7 +209,7 @@ int main(void)
 	ssd1306_init();
 
 	display_clear();
-	display_draw_string(19,10,"EmbedoS",0);
+	display_draw_string(19,10,"EmbedoS1",0);
 	display_draw_string(35,40,"OLED",0);
 	ssd1306_draw_buffer(display_buffer);
 	_delay_ms(100);
